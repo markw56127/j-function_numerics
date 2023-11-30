fe19d6ccca846b16e8b60f2d0899289284caf31b
markw56127 <48261076+markw56127@users.noreply.github.com>
2023-11-30 14:17:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from mpmath import *
 import math
 
 def comp_jtau(tau):
-    big_J = kleinj(tau) # imports the J-func
-    small_j = 1728 * big_J # big J into little j
+    big_J = kleinj(tau)
+    small_j = 1728 * big_J
     return small_j
 
-def discriminant(B): # modulo arithmetic for discriminant (for values of B) and for constant c
+def discriminant(B):
     if B % 4 == 1:
         disc = B
     else:
@@ -17,33 +17,33 @@ def discriminant(B): # modulo arithmetic for discriminant (for values of B) and 
         c = 1
     return disc, c
 
-def u_sol(n,c): # how does it work for n = 0 (mod 4)
+def u_sol(n,c):
     '''Iterating to find the lowest b value that gives a perfect square'''
     b_lst = []
     b = 1
     while math.sqrt(n*(b**2)+(c**2)) != math.isqrt(n*(b**2)+(c**2)):
         b += 1
-    b_lst.append(b) # adds the b found in the + version to a list
+    b_lst.append(b)
 
     b = 1
     while math.sqrt(n*(b**2)-(c**2)) != math.isqrt(n*(b**2)-(c**2)):
         b += 1
-    b_lst.append(b) # adds the b found in the - version to a list
+    b_lst.append(b)
 
-    b = min(b_lst) # gives the lowest value of b that satisfies the perfect square
+    b = min(b_lst)
 
-    a_1 = math.sqrt(n*(b**2)+(c**2)) # solves for a_1
+    a_1 = math.sqrt(n*(b**2)+(c**2))
     a_2 = math.sqrt(n*(b**2)-(c**2))
 
-    if a_1.is_integer(): # gives the positive integer a_1
+    if a_1.is_integer():
         a = a_1
     elif a_2.is_integer():
         a = a_2
 
-    u = (1/c)*(a+(b*math.sqrt(n))) # u_1 formula
+    u = (1/c)*(a+(b*math.sqrt(n)))
     return u
 
-def epsilon(u): # calculates the log(u_1)/log(epsilon)
+def epsilon(u):
     return math.log(u)
 
 print(epsilon(u_sol(53,2))) # works for D=5, but is off by a factor of 2 for otherwise
